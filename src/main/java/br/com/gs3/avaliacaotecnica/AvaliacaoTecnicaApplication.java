c7ef6423a160cffba952f40afed5eafb358d90c4
marcus.v.pereira <marcus.v.pereira@pbh.gov.br>
2021-10-22 20:12:27 -0300

    Criação do projeto SPRING, utilizando o SPRING.INITIALIZR

---
+++ src/main/java/br/com/gs3/avaliacaotecnica/AvaliacaoTecnicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.gs3.avaliacaotecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoTecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gs3/avaliacaotecnica/AvaliacaoTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gs3.avaliacaotecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
